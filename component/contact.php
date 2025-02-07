f142b1790eed61a4b120d90bb7536bd6f200c6aa
florian.llimos <florian.llimos@digitanie.org>
2025-02-07 16:14:12 +0100

    feat: add the structure for the contact section

---
+++ component/contact.php
@@ -0,0 +1,11 @@
+<div id="contact">
+  <h2 class="title-section">Contact</h2>
+  <div class="container-contact">
+    <div class="container-cv" id="cv">
+      <div class="contact-container-cv">
+        <button id="download1" class="download-dark" title="Télécharger mon cv" tabindex="0">Télécharger CV</button>
+      </div>
+      <img src="media/cv.png" alt="CV de Florian Llimos">
+    </div>
+  </div>
+</div>
