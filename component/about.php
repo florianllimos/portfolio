13a740ccb9304c4d37d5f204f7c246f902ed6710
florian.llimos <florian.llimos@digitanie.org>
2025-02-07 16:16:32 +0100

    feat: structure for the about section

---
+++ component/about.php
@@ -0,0 +1,54 @@
+<div id="about" role="region" aria-labelledby="title-about">
+  <h2 class="title-section" id="title-about">À propos de moi</h2>
+  <div class="container-about">
+    <div class="container-about-bloc" tabindex="0" role="article" aria-labelledby="about-presentation">
+      <div class="container-about-bloc-number section" aria-hidden="true">
+        <span class="container-about-bloc-number-span">1</span>
+      </div>
+      <img src="media/about/selfi.png" alt="Florian Llimos, développeur web" class="container-about-logo section">
+      <p class="color-dark section" id="about-presentation">
+        Je m'appelle Florian, j'ai 25 ans, et je suis un développeur web passionné basé près de Toulouse. Depuis mes débuts il y a quelques années, j'ai travaillé dur pour acquérir des compétences solides et une bonne maîtrise des technologies web modernes.
+      </p>
+    </div>
+
+    <div class="container-about-bloc" tabindex="0" role="article" aria-labelledby="about-diplome">
+      <div class="container-about-bloc-number section" aria-hidden="true">
+        <span class="container-about-bloc-number-span">2</span>
+      </div>
+      <img src="media/about/diplome.png" alt="Diplôme en développement web" class="container-about-logo section">
+      <p class="color-dark section" id="about-diplome">
+        Passionné de programmation web, j'ai suivi une formation qui m'a permis de maîtriser HTML, CSS, JavaScript, PHP, ainsi que des frameworks comme React et Leaflet. J'ai aussi acquis de l'expérience dans la création de sites réactifs et conviviaux.
+      </p>
+    </div>
+
+    <div class="container-about-bloc" tabindex="0" role="article" aria-labelledby="about-passion">
+      <div class="container-about-bloc-number section" aria-hidden="true">
+        <span class="container-about-bloc-number-span">3</span>
+      </div>
+      <img src="media/about/passion.png" alt="Passion pour le développement web" class="container-about-logo section">
+      <p class="color-dark section" id="about-passion">
+        Ce qui me motive dans le développement web, c'est de créer des expériences en ligne répondant aux besoins des utilisateurs. J'aime résoudre des problèmes complexes, concevoir des interfaces intuitives et rendre le web plus accessible.
+      </p>
+    </div>
+
+    <div class="container-about-bloc" tabindex="0" role="article" aria-labelledby="about-recherche">
+      <div class="container-about-bloc-number section" aria-hidden="true">
+        <span class="container-about-bloc-number-span">4</span>
+      </div>
+      <img src="media/about/vivre.png" alt="Expériences professionnelles en développement web" class="container-about-logo section">
+      <p class="color-dark section" id="about-recherche">
+        Au fil des ans, j'ai travaillé sur divers projets, de sites d'entreprises à des applications interactives, qui m'ont permis de progresser en tant que développeur. Je partage fièrement certains de ces projets dans mon portfolio.
+      </p>
+    </div>
+
+    <div class="container-about-bloc container-about-bloc-last" tabindex="0" role="article" aria-labelledby="about-idee">
+      <div class="container-about-bloc-number section" aria-hidden="true">
+        <span class="container-about-bloc-number-span">5</span>
+      </div>
+      <img src="media/about/idee.png" alt="Ouvert à de nouvelles opportunités" class="container-about-logo section">
+      <p class="color-dark section" id="about-idee">
+        Je suis actuellement à la recherche de nouvelles opportunités en développement web. Enthousiaste à l'idée de contribuer à des projets innovants, je souhaite collaborer avec des passionnés et continuer à développer mes compétences.
+      </p>
+    </div>
+  </div>
+</div>
