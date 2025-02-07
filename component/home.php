7fd628785914097fe8c4bd7b33b4079d69c520c4
florian.llimos <florian.llimos@digitanie.org>
2025-02-07 16:11:23 +0100

    feat: add structure html for the hero

---
+++ component/home.php
@@ -0,0 +1,60 @@
+<div id="home" role="region" aria-labelledby="home-hello-text">
+  <div class="container-typing">
+    <p id="typing">
+      <p><span class="orange">System</span>.<span class="red">out</span>.<span class="blue">println</span><span class="orange">("</span><span class="typed-text" aria-hidden="true"></span><span class="cursor" aria-hidden="true">&nbsp;</span><span class="orange">");</span></p>
+    </p>
+  </div>
+  <div class="container-home-hello">
+    <p id="home-hello-text"><span class="orange">echo</span> <span class="red">"Bonjour & bienvenue !"</span>;</p>
+  </div>
+  <div class="container-home-picture">
+    <img src="media/home/florian.png" alt="Photo de Florian Llimos" title="Photo de moi-même" id="home-picture">
+  </div>
+  <div class="container-home-download">
+    <button id="download" class="download-dark" title="Télécharger mon CV" tabindex="0" aria-label="Télécharger mon CV">Télécharger CV</button>
+  </div>
+  <div class="container-home-social-link" role="navigation" aria-label="Liens vers les réseaux sociaux">
+    <ul role="menu" class="ul-social-link">
+      <li role="menuitem">
+        <a href="https://www.instagram.com/f.lmss/?hl=fr" aria-label="Lien vers mon profil Instagram" title="Lien vers mon profil Instagram" target="_blank">
+          <img src="media/theme/dark/instagram.png" id="instagram" alt="Icône Instagram" title="Instagram">
+        </a>
+      </li>  
+      <li role="menuitem">
+        <a href="https://www.facebook.com/florian.llimos/" aria-label="Lien vers mon profil Facebook" title="Lien vers mon profil Facebook" target="_blank">
+          <img src="media/theme/dark/facebook.png" id="facebook" alt="Icône Facebook" title="Facebook">
+        </a>
+      </li>
+      <li role="menuitem">
+        <a href="https://fr.linkedin.com/in/florian-llimos-a27944231" aria-label="Lien vers mon profil LinkedIn" title="Lien vers mon profil LinkedIn" target="_blank">
+          <img src="media/theme/dark/linkedin.png" id="linkedin" alt="Icône LinkedIn" title="LinkedIn">
+        </a>
+      </li>
+      <li role="menuitem">
+        <a href="https://github.com/florianllimos" aria-label="Lien vers mon profil GitHub" title="Lien vers mon profil GitHub" target="_blank">
+          <img src="media/theme/dark/github.png" id="github" alt="Icône GitHub" title="GitHub">
+        </a>
+      </li>
+      <li role="menuitem">
+        <a href="https://codepen.io/llimosf-the-sasster" aria-label="Lien vers mon profil Codepen" title="Lien vers mon profil Codepen" target="_blank">
+          <img src="media/theme/dark/codepen.png" id="codepen" alt="Icône Codepen" title="Codepen">
+        </a>
+      </li>
+      <li role="menuitem">
+        <a href="tel:0622005584" aria-label="Lien pour me téléphoner" title="Lien pour me téléphoner">
+          <img src="media/theme/dark/telephone.png" id="telephone" alt="Icône téléphone" title="Téléphone">
+        </a>
+      </li>
+      <li role="menuitem">
+        <a href="mailto:llimosflorian1@gmail.com" aria-label="Lien pour m'envoyer un e-mail" title="Lien pour m'envoyer un e-mail">
+          <img src="media/theme/dark/mail.png" id="mail" alt="Icône e-mail" title="E-mail">
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="container-home-bottom">
+    <a href="#about" aria-label="Lien vers la section À propos">
+      <img src="media/theme/dark/arrow.png" alt="Flèche pointant vers le bas" id="home-bottom" class="home-bottom-dark" title="Lien vers À propos">
+    </a>
+  </div>
+</div>
